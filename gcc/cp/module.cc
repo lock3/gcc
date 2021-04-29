579c117df913a98f0d8f3b66a01bf74564e8cc4e
Tyler Sutton <tsutton@lock3software.com>
2021-04-29 10:16:57 -0400

    make tests run

--- gcc/cp/module.cc
+++ gcc/cp/module.cc
@@ -4050,8 +4050,8 @@ public:
   {
     if (!dumps || !dumps->stream)
       return false;
-//     if (mask && !(mask & flags))
-//       return false;
+    if (mask && !(mask & flags))
+      return false;
     return true;
   }
   /* Dump some information.  */
